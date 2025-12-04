921156eb62a5490ed3421f5e91d80a0f61c67572
DevPool1 <alex.tutunaru26@gmail.com>
2025-12-04 00:03:49 +0000

    feat : alteração do codigo para incluir imagens nas colunas. Closes #11

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Gestão de Utilizadores',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    imgSrc: require('@site/static/img/user_management.png').default,
     description: (
       <>
         Perfis seguros e personalizados para cada membro da equipa ou cliente.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Base de Dados de Alimentos',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    imgSrc: require('@site/static/img/food_database.png').default,
     description: (
       <>
         CRUD completo de alimentos. Adicione, edite e remova alimentos facilmente.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Diário Alimentar',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    imgSrc: require('@site/static/img/food_diary.png').default,
     description: (
       <>
         Registo diário e cálculo automático de calorias e macronutrientes.
@@ -32,7 +32,7 @@ const FeatureList = [
   },
   {
     title: 'Cálculo de IMC',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    imgSrc: require('@site/static/img/bmi_calculator.png').default,
     description: (
       <>
         Monitorização de saúde com cálculo automático do Índice de Massa Corporal.
@@ -41,11 +41,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ imgSrc, title, description }) {
   return (
     <div className={clsx('col col--3')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={imgSrc} className={styles.featureSvg} role="img" alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
